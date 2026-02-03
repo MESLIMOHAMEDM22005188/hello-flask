e326f5b9c51a9b5437070c4caadba19c35d7602d
MESLIMOHAMEDM22005188 <125298121+MESLIMOHAMEDM22005188@users.noreply.github.com>
2026-02-03 14:08:04 +0100

    Migrate from Express to Flask for routing

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+@app.route('/test')
+def test():
+    return 'Route test OK'
